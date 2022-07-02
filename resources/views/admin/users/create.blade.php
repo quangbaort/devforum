874224d364dde20bb76da77503fdd6d43d02ca23
Ngo Quoc Dat <datlechin@gmail.com>
2022-07-02 21:50:19 +0700

    create user page

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,5 +2,59 @@
 @section('content')
     <div class="container-fluid">
         {{ Breadcrumbs::render(__('Create user')) }}
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ route('admin.users.store') }}" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Họ tên</label>
+                                <input type="text" id="name" name="name" @class(['form-control', 'is-invalid' => $errors->has('name')]) placeholder="Họ tên người dùng">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Họ tên</label>
+                                <input type="email" id="email" name="email" @class(['form-control', 'is-invalid' => $errors->has('email')]) placeholder="Email người dùng">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Mật khẩu</label>
+                                <input type="password" id="password" name="password" @class(['form-control', 'is-invalid' => $errors->has('password')]) placeholder="Mật khẩu người dùng">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="password_confirmation" class="form-label">Xác nhận mật khẩu</label>
+                                <input type="password" id="password_confirmation" name="password_confirmation" @class(['form-control', 'is-invalid' => $errors->has('password_confirmation')]) placeholder="Xác nhận mật khẩu người dùng">
+                                @error('password_confirmation')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check form-switch form-switch-danger">
+                                    <input class="form-check-input" type="checkbox" id="role">
+                                    <label class="form-check-label" for="role">Super Admin</label>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Tạo mới</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
